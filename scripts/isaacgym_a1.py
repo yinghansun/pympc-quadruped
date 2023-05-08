3bf5d26580f6c8b3a93feddd7b551631d0b6171a
yinghansun <yinghansun2@gmail.com>
2023-05-08 20:47:58 +0800

    add multi-robots support

--- scripts/isaacgym_a1.py
+++ scripts/isaacgym_a1.py
@@ -1,5 +1,6 @@
 import os
 import sys
+from typing import List
 sys.path.append(os.path.join(os.path.dirname(__file__), '../config'))
 sys.path.append(os.path.join(os.path.dirname(__file__), '../linear_mpc'))
 sys.path.append(os.path.join(os.path.dirname(__file__), '../utils/'))
@@ -17,6 +18,7 @@ from robot_data import RobotData
 from swing_foot_trajectory_generator import SwingFootTrajectoryGenerator
 
 
+NUM_ROBOTS = 4
 STATE_ESTIMATION = False
 
 def reset(gym, sim):
@@ -60,37 +62,45 @@ def main():
         use_gpu_pipeline=True
     )
 
-    asset_root_path = os.path.join(os.path.dirname(__file__), '../robot/')
-    robot_path = 'a1/urdf/a1.urdf'
-    robot_model = load_model(gym, sim, asset_root_path, robot_path, is_base_fixed=False)
-
     robot_config = A1Config
 
+    asset_root_path = os.path.join(os.path.dirname(__file__), '../robot/')
+    robot_path = 'a1/urdf/a1.urdf'
     urdf_path = os.path.join(asset_root_path, robot_path)
-    robot_data = RobotData(urdf_path, state_estimation=STATE_ESTIMATION)
+    
+    l_robot_model = []
+    l_robot_data: List[RobotData] = []
+    for _ in range(NUM_ROBOTS):
+        l_robot_model.append(load_model(gym, sim, asset_root_path, robot_path, is_base_fixed=False))
+        l_robot_data.append(RobotData(urdf_path, state_estimation=STATE_ESTIMATION))
 
     creat_ground_plane(gym, sim)
 
-    envs, actors = create_envs_actors(gym, sim, robot_model, robot_config.base_height_des)
+    for robot_model in l_robot_model:
+        envs, actors = create_envs_actors(gym, sim, robot_model, robot_config.base_height_des)
 
     viewer = add_viewer(gym, sim)
 
     gym.prepare_sim(sim)
 
-    reset(gym, sim)
+    # reset(gym, sim)
 
-
-    predictive_controller = ModelPredictiveController(LinearMpcConfig, A1Config)
-    leg_controller = LegController(robot_config.Kp_swing, robot_config.Kd_swing)
-
-    gait = Gait.TROTTING10
-    swing_foot_trajs = [SwingFootTrajectoryGenerator(leg_idx) for leg_idx in range(4)]
+    l_predictive_controller: List[ModelPredictiveController] = []
+    l_leg_controller: List[LegController] = []
+    l_gait: List[Gait] = []
+    l_swing_foot_trajs: List[SwingFootTrajectoryGenerator] = []
+    for _ in range(NUM_ROBOTS):
+        l_predictive_controller.append(ModelPredictiveController(LinearMpcConfig, A1Config))
+        l_leg_controller.append(LegController(robot_config.Kp_swing, robot_config.Kd_swing))
+        l_gait.append(Gait.TROTTING10)
+        l_swing_foot_trajs.append([SwingFootTrajectoryGenerator(leg_idx) for leg_idx in range(4)])
 
     vel_base_des = np.array([1.4, 0., 0.])
     yaw_turn_rate_des = 0.
 
     iter_counter = 0
     render_counter = 0
+    torque_cmds_tensor = torch.zeros(NUM_ROBOTS, 12, device='cuda:0', dtype=torch.float32)
 
     while not gym.query_viewer_has_closed(viewer):
         # step graphics
@@ -106,50 +116,50 @@ def main():
         joint_state_tensor_discription = gym.acquire_dof_state_tensor(sim)
         joint_state_tensor: torch.Tensor = gymtorch.wrap_tensor(joint_state_tensor_discription)
 
-        quat_base_imre=base_state_tensor[0, 3:7].cpu().numpy()
-        quat_base_reim = np.array(
-            [quat_base_imre[3], quat_base_imre[0], quat_base_imre[1], quat_base_imre[2]],
-            dtype=np.float32
-        )
-        robot_data.update(
-            pos_base=base_state_tensor[0, 0:3].cpu().numpy(),
-            quat_base=quat_base_reim,
-            lin_vel_base=base_state_tensor[0, 7:10].cpu().numpy(),
-            ang_vel_base=base_state_tensor[0, 10:13].cpu().numpy(),
-            q = joint_state_tensor[:, 0].cpu().numpy(),
-            qdot = joint_state_tensor[:, 1].cpu().numpy()
-        )
+        for robot_idx, (robot_data, predictive_controller, leg_controller, gait, swing_foot_trajs) \
+            in enumerate(zip(l_robot_data, l_predictive_controller, l_leg_controller, l_gait, l_swing_foot_trajs)):
+            quat_base_imre=base_state_tensor[robot_idx, 3:7].cpu().numpy()
+            quat_base_reim = np.array(
+                [quat_base_imre[3], quat_base_imre[0], quat_base_imre[1], quat_base_imre[2]],
+                dtype=np.float32
+            )
+            robot_data.update(
+                pos_base=base_state_tensor[robot_idx, 0:3].cpu().numpy(),
+                quat_base=quat_base_reim,
+                lin_vel_base=base_state_tensor[robot_idx, 7:10].cpu().numpy(),
+                ang_vel_base=base_state_tensor[robot_idx, 10:13].cpu().numpy(),
+                q = joint_state_tensor[12*robot_idx:12*(robot_idx+1), 0].cpu().numpy(),
+                qdot = joint_state_tensor[12*robot_idx:12*(robot_idx+1), 1].cpu().numpy()
+            )
 
-        gait.set_iteration(predictive_controller.iterations_between_mpc, iter_counter)
-        swing_states = gait.get_swing_state()
-        gait_table = gait.get_gait_table()
+            gait.set_iteration(predictive_controller.iterations_between_mpc, iter_counter)
+            swing_states = gait.get_swing_state()
+            gait_table = gait.get_gait_table()
 
-        predictive_controller.update_robot_state(robot_data)
+            predictive_controller.update_robot_state(robot_data)
 
-        contact_forces = predictive_controller.update_mpc_if_needed(iter_counter, vel_base_des, 
-            yaw_turn_rate_des, gait_table, solver='drake', debug=False, iter_debug=0) 
+            contact_forces = predictive_controller.update_mpc_if_needed(iter_counter, vel_base_des, 
+                yaw_turn_rate_des, gait_table, solver='drake', debug=False, iter_debug=0) 
 
-        print(contact_forces)
+            pos_targets_swingfeet = np.zeros((4, 3))
+            vel_targets_swingfeet = np.zeros((4, 3))
 
-        pos_targets_swingfeet = np.zeros((4, 3))
-        vel_targets_swingfeet = np.zeros((4, 3))
-
-        for leg_idx in range(4):
-            if swing_states[leg_idx] > 0:   # leg is in swing state
-                swing_foot_trajs[leg_idx].set_foot_placement(
-                    robot_data, gait, vel_base_des, yaw_turn_rate_des
-                )
-                base_pos_base_swingfoot_des, base_vel_base_swingfoot_des = \
-                    swing_foot_trajs[leg_idx].compute_traj_swingfoot(
-                        robot_data, gait
+            for leg_idx in range(4):
+                if swing_states[leg_idx] > 0:   # leg is in swing state
+                    swing_foot_trajs[leg_idx].set_foot_placement(
+                        robot_data, gait, vel_base_des, yaw_turn_rate_des
                     )
-                pos_targets_swingfeet[leg_idx, :] = base_pos_base_swingfoot_des
-                vel_targets_swingfeet[leg_idx, :] = base_vel_base_swingfoot_des
+                    base_pos_base_swingfoot_des, base_vel_base_swingfoot_des = \
+                        swing_foot_trajs[leg_idx].compute_traj_swingfoot(
+                            robot_data, gait
+                        )
+                    pos_targets_swingfeet[leg_idx, :] = base_pos_base_swingfoot_des
+                    vel_targets_swingfeet[leg_idx, :] = base_vel_base_swingfoot_des
 
-        torque_cmds = leg_controller.update(robot_data, contact_forces, swing_states, pos_targets_swingfeet, vel_targets_swingfeet)
-        torque_cmds_tensor = torch.tensor(torque_cmds, dtype=torch.float32, device='cuda:0')
-        torque_cmds_tensor_discription = gymtorch.unwrap_tensor(torque_cmds_tensor)
-        gym.set_dof_actuation_force_tensor(sim, torque_cmds_tensor_discription)
+                torque_cmds = leg_controller.update(robot_data, contact_forces, swing_states, pos_targets_swingfeet, vel_targets_swingfeet)
+                torque_cmds_tensor[robot_idx, :] = torch.tensor(torque_cmds, dtype=torch.float32, device='cuda:0')
+            torque_cmds_tensor_discription = gymtorch.unwrap_tensor(torque_cmds_tensor)
+            gym.set_dof_actuation_force_tensor(sim, torque_cmds_tensor_discription)
 
         gym.fetch_results(sim, True)
         gym.simulate(sim)
@@ -158,6 +168,11 @@ def main():
         gym.refresh_actor_root_state_tensor(sim)
 
         iter_counter += 1
+
+        if iter_counter == 3000:
+            reset(gym, sim)
+            iter_counter = 0
+            render_counter = 0
 
     gym.destroy_viewer(viewer)
     gym.destroy_sim(sim)
